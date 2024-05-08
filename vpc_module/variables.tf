5c2808063b2ab96e2bd6f17dd83fa0f25ecd83dd
tereza-arta <157483329+tereza-arta@users.noreply.github.com>
2024-05-08 12:04:36 +0400

    Update child variables.tf

--- vpc_module/variables.tf
+++ vpc_module/variables.tf
@@ -41,34 +41,3 @@ variable "default_gateway" {
   description = "Gateway for access to the internet"
   default     = "0.0.0.0/0"
 }
-
-#Variables for Instance
-variable "ami" {
-  type        = string
-  description = "Ami_id of instance"
-  default     = "ami-0a7187996cbd8989f"
-}
-
-variable "instance_type" {
-  type        = string
-  description = "Type of EC2 instance(CPU,RAM,Disk,Network components)"
-  default     = "t3.micro"
-}
-
-variable "pub_ec2_tag" {
-	type        = map(string)
-	description = "Tag of ec2 in pub-sub"
-	default     = {
-		Name = "Custom EC2 first"
-		Tag  = "Instance in public subnet" 
-	}  
-}
-
-variable "priv_ec2_tag" {
-  type        = map(string)
-  description = "Tag of ec2 in priv-sub"
-  default     = {
-    Name = "Custom EC2 second"
-    Tag  = "Instance in private subnet"
-  }
-}
